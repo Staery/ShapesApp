13e7f3b0232ace0c285aaa21a614daae6591bd24
Anton <anton.selkin@gmail.com>
2024-06-13 07:09:05 +0300

    Create shapes model

---
+++ ShapesApp/Models/Rectangle.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace ShapesApp.Models
+{
+    public class Rectangle
+    {
+        public Color Color { get; set; }
+        public Point BottomLeft { get; set; }
+        public Point BottomRight { get; set; }
+        public Point TopLeft { get; set; }
+        public Point TopRight { get; set; }
+    }
+}
